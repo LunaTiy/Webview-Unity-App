444efd0680067669b84fed2517ea7b1a8695ed4b
Daniil Savushkin <savushkin-daniil-99@yandex.ru>
2023-04-10 23:44:29 +0300

    Add loading data in diary

---
+++ Assets/CodeBase/Infrastructure/Services/SaveLoad/ISaveProgressReader.cs
@@ -0,0 +1,9 @@
+﻿using CodeBase.Data;
+
+namespace CodeBase.Infrastructure.Services.SaveLoad
+{
+    public interface ISaveProgressReader
+    {
+        void Load(SavedData savedData);
+    }
+}
--- Assets/CodeBase/Logic/Diary/TrainingsPresenter.cs
+++ Assets/CodeBase/Logic/Diary/TrainingsPresenter.cs
@@ -1,9 +1,12 @@
-﻿using CodeBase.Data.Diary;
+﻿using System.Collections.Generic;
+using CodeBase.Data;
+using CodeBase.Data.Diary;
+using CodeBase.Infrastructure.Services.SaveLoad;
 using UnityEngine;
 
 namespace CodeBase.Logic.Diary
 {
-    public class TrainingsPresenter : MonoBehaviour
+    public class TrainingsPresenter : MonoBehaviour, ISaveProgressReader
     {
         [SerializeField] private TrainingPresenter _trainingPresenterPrefab;
         [SerializeField] private TrainingFactory _trainingFactory;
@@ -15,7 +18,18 @@ namespace CodeBase.Logic.Diary
         private void OnDisable() => 
             _trainingFactory.OnTrainingCreated -= TrainingCreatedHandler;
 
-        private void TrainingCreatedHandler(Training training)
+        public void Load(SavedData savedData)
+        {
+            List<Training> trainings = savedData.trainingDiary.trainings;
+            
+            for (int i = trainings.Count - 1; i > 0; i--) 
+                Create(trainings[i]);
+        }
+
+        private void TrainingCreatedHandler(Training training) => 
+            Create(training);
+
+        private void Create(Training training)
         {
             TrainingPresenter trainingUi = Instantiate(_trainingPresenterPrefab, _trainingsContentRoot);
             trainingUi.transform.SetSiblingIndex(0);
